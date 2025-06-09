c09bc529ca1b214f50b6ab33954fc4c6e15658a9
Rahulkumarbhagat1 <93385089+Rahulkumarbhagat1@users.noreply.github.com>
2025-06-09 19:21:46 +0530

    Add files via upload

---
+++ TestNg/src/test/java/com/example/tests/FirstClass.java
@@ -0,0 +1,17 @@
+package com.example.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class FirstClass {
+	
+	  @Test
+	    public void testSubtraction() {
+	        int a = 10;
+	        int b = 5;
+	        int expectedDifference = 5;
+	        int actualDifference = a - b;
+	        Assert.assertEquals(actualDifference, expectedDifference, "Difference should be 5");
+	    }
+
+}
